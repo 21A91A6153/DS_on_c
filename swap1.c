c104e58eb246e28befe665f23b7f2c5c293795ac
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-07-21 11:53:21 +0530

    Add files via upload

---
+++ swap1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void swap(int ,int);
+void main()
+{
+	int a=10,b=20;
+	printf("before swapping a=%d ,b=%d\n",a,b);
+	swap(a,b);
+	printf("after swapping a=%d, b=%d\n",a,b);
+}
+void swap(int a,int b)
+{
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+	printf("after swapping in fun a=%d b=%d\n",a,b);
+}
